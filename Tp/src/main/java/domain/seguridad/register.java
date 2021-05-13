0cb1ec98f9ddfdd4eab7db3b1ed5cbfd1f7d9d30
Tomas Molino <59565479+totomolino@users.noreply.github.com>
2021-05-12 21:40:03 -0300

    Separe las clases en archivos distintos

--- Tp/src/main/java/domain/seguridad/register.java
+++ Tp/src/main/java/domain/seguridad/register.java
@@ -7,10 +7,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class register {
-    /*
-    public static boolean existeUsuario(String usuario){
-    }
-    */
 
     public static boolean esFacil(String unaContrasenia) throws FileNotFoundException {
 
@@ -39,12 +35,6 @@ public class register {
         System.out.println("Ingrese un Usuario: ");
         String usuario= entrada.nextLine();
 
-        /*
-        if(!register.existeUsuario(usuario)){
-
-        }
-        */
-
         System.out.println("Ingrese una contrasenia: ");
         String contra = entrada.nextLine();
 
@@ -53,93 +43,6 @@ public class register {
             contra = entrada.nextLine();
         }
     }
-}
-
-class reglasNist {
-
-    static int largoMinimo = 8;
-    static int largoMaximo = 64;
-
-    static boolean cumpleLasReglas(String unaContrasenia, String unUsuario) {
-
-
-        return reglasNist.cumpleLargo(unaContrasenia) &&
-                reglasNist.usuarioYContraDistintos(unaContrasenia, unUsuario) &&
-                !(reglasNist.mismoCaracterRepetido(unaContrasenia)) &&
-                !(reglasNist.esIncremental(unaContrasenia)) &&
-                !(reglasNist.esDecremental(unaContrasenia)) &&
-                cumpleReglasSintacticas(unaContrasenia);
-    }
-
-    static boolean cumpleLargo(String unaContrasenia) {
-
-        int longitud = unaContrasenia.length();
-
-        return (largoMinimo <= longitud && longitud <= largoMaximo);
-    }
-
-    static boolean usuarioYContraDistintos(String unaContrasenia, String unUsuario) {
-        return !(unUsuario.equals(unaContrasenia));
-    }
-
-    static boolean mismoCaracterRepetido(String unaContrasenia) {
-
-        char[] ArrayContrasenia = unaContrasenia.toCharArray();
-        char primerCaracter = ArrayContrasenia[0];
-        int i = 0, largoContrasenia = unaContrasenia.length();
-
-
-        while (i < largoContrasenia && primerCaracter == ArrayContrasenia[i]) {
-            i++;
-        }
-        return largoContrasenia == i;
-    }
-
-    static boolean esIncremental(String unaContrasenia) {
-
-        char[] ArrayContrasenia = unaContrasenia.toCharArray();
-        char primerCaracter = ArrayContrasenia[0];
-        int i = 0, largoContrasenia = unaContrasenia.length();
-
-
-        while (i < largoContrasenia && (primerCaracter + i) == ArrayContrasenia[i]) {
-            i++;
-        }
-        return largoContrasenia == i;
-    }
-
-    static boolean esDecremental(String unaContrasenia) {
-
-        char[] ArrayContrasenia = unaContrasenia.toCharArray();
-        char primerCaracter = ArrayContrasenia[0];
-        int i = 0, largoContrasenia = unaContrasenia.length();
-
-
-        while (i < largoContrasenia && (primerCaracter - i) == ArrayContrasenia[i]) {
-            i++;
-        }
-        return largoContrasenia == i;
-    }
-
-    static boolean cumpleReglasSintacticas(String unaContrasenia) {
-        char clave;
-        int contNumero = 0, contLetraMay = 0, contLetraMin = 0;
-
-        for (int i = 0; i < unaContrasenia.length(); i++) {
-            clave = unaContrasenia.charAt(i);
-            String passValue = String.valueOf(clave);
-            if (passValue.matches("[A-Z]")) {
-                contLetraMay++;
-            } else if (passValue.matches("[a-z]")) {
-                contLetraMin++;
-            } else if (passValue.matches("[0-9]")) {
-                contNumero++;
-            }
-        }
-
-        return (contNumero != 0) && (contLetraMay != 0) && (contLetraMin != 0);
-    }
-
 }
 
 
---
+++ Tp/src/main/java/domain/seguridad/reglasNist.java
@@ -0,0 +1,88 @@
+package domain.seguridad;
+
+public class reglasNist {
+
+    static int largoMinimo = 8;
+    static int largoMaximo = 64;
+
+    static boolean cumpleLasReglas(String unaContrasenia, String unUsuario) {
+
+
+        return reglasNist.cumpleLargo(unaContrasenia) &&
+                reglasNist.usuarioYContraDistintos(unaContrasenia, unUsuario) &&
+                !(reglasNist.mismoCaracterRepetido(unaContrasenia)) &&
+                !(reglasNist.esIncremental(unaContrasenia)) &&
+                !(reglasNist.esDecremental(unaContrasenia)) &&
+                cumpleReglasSintacticas(unaContrasenia);
+    }
+
+    static boolean cumpleLargo(String unaContrasenia) {
+
+        int longitud = unaContrasenia.length();
+
+        return (largoMinimo <= longitud && longitud <= largoMaximo);
+    }
+
+    static boolean usuarioYContraDistintos(String unaContrasenia, String unUsuario) {
+        return !(unUsuario.equals(unaContrasenia));
+    }
+
+    static boolean mismoCaracterRepetido(String unaContrasenia) {
+
+        char[] ArrayContrasenia = unaContrasenia.toCharArray();
+        char primerCaracter = ArrayContrasenia[0];
+        int i = 0, largoContrasenia = unaContrasenia.length();
+
+
+        while (i < largoContrasenia && primerCaracter == ArrayContrasenia[i]) {
+            i++;
+        }
+        return largoContrasenia == i;
+    }
+
+    static boolean esIncremental(String unaContrasenia) {
+
+        char[] ArrayContrasenia = unaContrasenia.toCharArray();
+        char primerCaracter = ArrayContrasenia[0];
+        int i = 0, largoContrasenia = unaContrasenia.length();
+
+
+        while (i < largoContrasenia && (primerCaracter + i) == ArrayContrasenia[i]) {
+            i++;
+        }
+        return largoContrasenia == i;
+    }
+
+    static boolean esDecremental(String unaContrasenia) {
+
+        char[] ArrayContrasenia = unaContrasenia.toCharArray();
+        char primerCaracter = ArrayContrasenia[0];
+        int i = 0, largoContrasenia = unaContrasenia.length();
+
+
+        while (i < largoContrasenia && (primerCaracter - i) == ArrayContrasenia[i]) {
+            i++;
+        }
+        return largoContrasenia == i;
+    }
+
+    static boolean cumpleReglasSintacticas(String unaContrasenia) {
+        char clave;
+        int contNumero = 0, contLetraMay = 0, contLetraMin = 0;
+
+        for (int i = 0; i < unaContrasenia.length(); i++) {
+            clave = unaContrasenia.charAt(i);
+            String passValue = String.valueOf(clave);
+            if (passValue.matches("[A-Z]")) {
+                contLetraMay++;
+            } else if (passValue.matches("[a-z]")) {
+                contLetraMin++;
+            } else if (passValue.matches("[0-9]")) {
+                contNumero++;
+            }
+        }
+
+        return (contNumero != 0) && (contLetraMay != 0) && (contLetraMin != 0);
+    }
+
+}
