b0f0c6432cd2da4fd2b4c5bf8d86c5f22cb5f493
Tomas Molino <59565479+totomolino@users.noreply.github.com>
2021-11-06 19:09:21 -0300

    No se como alinear los radios de tipo usuario

--- Front/jscript/registrarPersona.js
+++ Front/jscript/registrarPersona.js
@@ -9,6 +9,49 @@ var app = new Vue({
         numero: "",
         telefono: "",
         email:"",
-        formasNotif: []      
+        formasNotif: [],
+        username:"",
+        password:"",
+        password2:"",
+        tipo:"",
+        nombreCon : "",
+        apellidoCon : "",
+        fechaDeNacimientoCon : "",
+        documentoCon: "",
+        numeroCon: "",
+        telefonoCon: "",
+        emailCon:"",
+        formasNotifCon: [],             
+    },
+    methods:{
+        registrarse: function(){
+            
+            //primero se crea el usuario
+
+
+
+
+
+
+
+        }
     }
 })
+
+
+function crearUsuario(usuario, email, contrasenia, tipo){
+
+    var req = {
+        "usu_email": email,
+        "usu_contrasena":contrasenia,
+        "usu_nombre": usuario,
+        "usu_tipo": tipo   
+    }
+
+    fetch("http://localhost:4567/patitas/user", {
+
+        method: "POST",
+        body: JSON.stringify(req)
+    })
+
+}
